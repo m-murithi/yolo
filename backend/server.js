911375f425735fc7d34104aaf23f691d6f960e68
Marvin <50557936+m-murithi@users.noreply.github.com>
2024-05-02 11:50:56 +0000

    fix: cors error

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,10 @@ app.use(express.json())
 app.use(upload.array()); 
 
 // Cors 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 // Use Route
 app.use('/api/products', productRoute)
